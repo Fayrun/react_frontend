eee5578af1d9b13abf9a2d33bf7dd5fdf961108d
Fayrun <luuphuc1368@gmail.com>
2025-11-05 09:04:22 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Content from "./Content";
+
+function App() {
+  return (
+    <div style={{ padding: "32px" }}>
+      <Content ></Content>
+    </div>
+  );
+}
+
+export default App;
